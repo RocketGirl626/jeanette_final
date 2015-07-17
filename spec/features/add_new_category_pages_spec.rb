37861c75cf422afc21328ba19817745e49962cc2
Jeanette Fairless <jfairless@spectrumhomeservices.com>
2015-07-16 21:01:25 -0700

    add all authorizations working, still some problems with specs

--- spec/features/add_new_category_pages_spec.rb
+++ spec/features/add_new_category_pages_spec.rb
@@ -5,13 +5,14 @@ describe "the add a Category process" do
     visit categories_path
     click_link 'Add a New Skill Category'
     fill_in 'Name', :with => 'Java'
-    # click_on 'Create Category'
-    # expect(page).to have_content 'Categories'
+    click_on 'Create Category'
+    expect(page).to have_content 'Categories'
   end
-  # it "gives an error when no data is entered into form fields" do
-  #   visit new_category_path
-  #   click_on 'Create Category'
-  #   expect(page).to have_content 'errors'
-  # end
+
+  it "gives an error when no data is entered into form fields" do
+    visit new_category_path
+    click_on 'Create Category'
+    expect(page).to have_content 'errors'
+   end
 
 end
